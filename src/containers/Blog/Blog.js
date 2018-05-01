b4ebfc92df64546b5982b27b1e43ffa40ec9396a
jock-dalby <jockdalby86@gmail.com>
2018-05-01 18:39:32 +1200

    move FullPost and NewPost to containers folder and extrapolate out Posts section to it's own container

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,47 +1,10 @@
 import React, { Component } from 'react';
-import instance from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from '../Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPost: null,
-        error: false
-    }
-
-    componentDidMount() {
-        instance.get('/posts')
-            .then(res => {
-                const posts = res.data.slice(0, 4).map(post => (
-                    {...post, author: 'Jock'}
-                ));
-                this.setState({
-                    posts,
-                    error: false
-                });
-            })
-            .catch(err => this.setState({error: true}));
-    }
-
-    postSelected(id) {
-        this.setState({
-            selectedPost: id
-        });
-    }
 
     render () {
-        let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(
-                post => <Post key={post.id}
-                            title={post.title}
-                            author={post.author}
-                            onClicked={ () => this.postSelected(post.id)}/>);
-        }
 
         return (
             <div className="Blog">
@@ -53,15 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPost}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
     }
---
+++ src/containers/Posts/Posts.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import instance from '../../axios';
+import Post from '../../components/Post/Post';
+
+class Posts extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    instance.get('/posts')
+      .then(res => {
+        const posts = res.data.slice(0, 4).map(post => (
+          { ...post, author: 'Jock' }
+        ));
+        this.setState({
+          posts
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        // this.setState({ error: true })
+      });
+  }
+
+  postSelected(id) {
+    this.setState({
+      selectedPost: id
+    });
+  }
+
+  render() {
+
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map(
+        post => <Post key={post.id}
+          title={post.title}
+          author={post.author}
+          onClicked={() => this.postSelected(post.id)} />);
+    }
+    return (
+      <section className="Posts">
+        {posts}
+      </section>
+    )
+  }
+}
+
+export default Posts;
