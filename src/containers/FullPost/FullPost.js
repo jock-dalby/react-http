bca2c045df11874d16a2812dedf0cf64fbbbdd0b
jock-dalby <jockdalby86@gmail.com>
2018-05-07 19:11:17 +1200

    access post id in componentDidMount from route params to load FullPost

--- src/containers/FullPost/FullPost.js
+++ src/containers/FullPost/FullPost.js
@@ -10,22 +10,22 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    componentDidUpdate() {
-        if (this.props.id) {
-            if (!this.state.loadedPost || this.state.loadedPost.id !== this.props.id) {
-                axios.get(`/posts/${this.props.id}`)
+    componentDidMount() {
+        if (this.props.match.params.id) {
+            if (!this.state.loadedPost || this.state.loadedPost.id !== this.props.match.params.id) {
+                axios.get(`/posts/${this.props.match.params.id}`)
                     .then(res => this.setState({ loadedPost: res.data }));
             }
         }
     }
 
     deletePostHandler = () => {
-        axios.delete(`/posts/${this.props.id}`)
+        axios.delete(`/posts/${this.props.match.params.id}`)
             .then(res => console.log('Deleted item:', res));
     }
 
     render () {
-        let post = <p style={{ textAlign: 'center' }}>{this.props.id ? 'loading...' : 'Please select a Post!'}</p>;
+        let post = <p style={{ textAlign: 'center' }}>{this.props.match.params.id ? 'loading...' : 'Please select a Post!'}</p>;
         if ( this.state.loadedPost ) {
             post = (
                 <div className="FullPost">
